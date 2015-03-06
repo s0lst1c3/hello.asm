21dc8eb0245f6c832f9210b474cb5d107af74775
= <=>
2015-03-06 01:22:34 -0500

    asdf

---
+++ hello.asm
@@ -0,0 +1,30 @@
+; Filename: hello.asm
+; Author: s0lst1c3
+; Description: writes "Hello!" on the screen using only system calls.
+
+
+section .text
+
+	global _start
+
+_start:
+
+	; system_call(arg, arg, arg) <----> eax(edi, esi, edx)
+	; write(1, message, 13)
+	mov eax, 0x1
+	mov edi, 0x1 ; 0x1 for stdout
+	mov esi, message
+	mov edx, 0xd
+	int 0x80
+
+	; exit(0)
+	mov eax, 0x3c
+	mov edi, 0x0
+	int 0x80
+	
+
+
+message:
+
+	db "Hello!", 0xa
+
